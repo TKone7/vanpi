68d7cf6de0a2589a9864f1e876cefd7f55b642e0
Vincent <vincent@pekaway.de>
2022-05-19 12:16:10 +0000

    vanpi installation script for clean raspi os bullseye lite

--- pi4_bullseye/vanpi-init.sh
+++ pi4_bullseye/vanpi-init.sh
@@ -26,7 +26,7 @@ echo -e "dtoverlay=w1-gpio\ndtoverlay=uart5\nenable_uart=1" | sudo tee -a /boot/
 
 # Set hostname to vanpi
 echo -e "${Cyan}Set hostname to vanpi${NC}"
-echo "hostname" | sudo tee /etc/hostname
+echo "vanpi" | sudo tee /etc/hostname
 
 # saving needed resources to ~/pekaway
 echo -e "${Cyan}saving needed resources to ~/pekaway${NC}"
