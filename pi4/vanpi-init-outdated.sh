db5c3d3878ec9141249a923375ba784ffc2b3247
Vincent <vincent@pekaway.de>
2023-05-03 15:32:51 +0000

    updated to v1.1.1

--- pi4/vanpi-init-outdated.sh
+++ pi4/vanpi-init-outdated.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Create logfile and make it writable
+#########################################################
+#														#
+# This script will install VanPi OS				 		#
+# on a clean and fresh 'Raspberry Pi OS (Buster)'!		#
+# Simply download the script and execute it.			#
+#														#
+#########################################################
+
+# create logfile and make it writable
 LOG_FILE=/var/log/pekaway-install.log
 sudo touch ${LOG_FILE}
 sudo chmod 0666 ${LOG_FILE}
@@ -9,7 +17,7 @@ exec 3<&1
 coproc mytee { tee ${LOG_FILE} >&3;  }
 exec >&${mytee[1]} 2>&1
 
-#define startdate
+# define startdate
 start=`date +%s`
 startdate=`date`
 
@@ -32,7 +40,7 @@ sudo apt update
 echo -e "${Cyan}upgrading packages${NC}"
 sudo apt upgrade -y
 
-# Enable I2C and 1-Wire
+# enable I2C and 1-Wire
 echo -e "${Cyan}Enabling I2C and 1-Wire Bus${NC}"
 sudo raspi-config nonint do_i2c 0
 echo -e "dtoverlay=w1-gpio\ndtoverlay=uart5\nenable_uart=1" | sudo tee -a /boot/config.txt
@@ -41,11 +49,11 @@ iptables-persistent iptables-persistent/autosave_v4 boolean true
 iptables-persistent iptables-persistent/autosave_v6 boolean true
 EOF
 
-# Set hostname to pekaway
+# set hostname to pekaway
 echo -e "${Cyan}Set hostname to pekaway${NC}"
 echo "pekaway" | sudo tee /etc/hostname
 
-# saving needed resources to ~/pekaway
+# save needed resources to ~/pekaway
 echo -e "${Cyan}saving needed resources to ~/pekaway${NC}"
 mkdir ~/pekaway
 cd pekaway
@@ -57,15 +65,6 @@ chmod 0744 ~/pekaway/availableWifi.txt
 chmod 0777 ~/pekaway/pythonsqlite.db
 chmod 0755 ~/pekaway/raspi-blinka.py
 echo "yes" > ~/pekaway/firstboot
-
-#install wiringpi
-echo -e "${Cyan}Installing/updating wiringpi...${NC}"
-cd /tmp
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-sudo dpkg -i wiringpi-latest.deb
-sudo sed -i 's/PIN = 5/PIN = 4/g' ~/pekaway/433Utils/RPi_utils/codesend.cpp
-sudo sed -i 's/PIN = 4/PIN = 3/g' ~/pekaway/433Utils/RPi_utils/RFSniffer.cpp
-cd ~/pekaway/433Utils/RPi_utils/ && make
 
 # get and install needed packages list
 echo -e "${Cyan}Saving list with needed packages${NC}"
@@ -86,30 +85,39 @@ else
 fi
 wget ${Server}pip3list.txt
 sudo apt install python3-pip -y
-# Install python modules locally (user vanpi) and globally (root)
+
+# install python modules locally (user pi) and globally (root)
 sudo pip3 install -r ~/pekaway/pip3list.txt
 sudo pip3 install bottle
 pip3 install -r ~/pekaway/pip3list.txt
 pip3 install bottle
 
-#install git
+# download files for NSPanel
+cd ~/pekaway
+wget ${Server}NSPanel/VanPI_NSPANEL.tft
+wget ${Server}NSPanel/autoexec.be
+mkdir -p ~/pekaway/userdata/NSPanel
+cp ~/pekaway/VanPI_NSPANEL.tft ~/pekaway/userdata/NSPanel/VanPI_NSPANEL.tft
+cd ~/
+
+# install git
 echo -e "${Cyan}Installing git${NC}"
 sudo apt install git make build-essential
 
-# install Node-Red including Node and npm
-echo -e "${Cyan}Installing/updating Node-Red, Node and npm${NC}"
-cd ~
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node16 --confirm-install --confirm-pi
+# install Node-RED including Node and npm
+echo -e "${Cyan}Installing/updating Node-RED, NodeJS and npm${NC}"
+cd ~/
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node18 --confirm-install --confirm-pi
 sudo systemctl enable nodered.service
-echo -e "${Cyan}Starting Node-Red for initial setup...${NC}"
+echo -e "${Cyan}Starting Node-RED for initial setup...${NC}"
 sudo systemctl start nodered.service
-echo -e "${Cyan}Sleeping for 20s to let Node-Red do it's thing...${NC}"
+echo -e "${Cyan}Sleeping for 20s to let Node-RED do it's thing...${NC}"
 sleep 20
-echo -e "${Cyan}Stopping Node-Red${NC}"
+echo -e "${Cyan}Stopping Node-RED${NC}"
 sudo systemctl stop nodered.service
 
-#install Node-Red modules
-echo -e "${Cyan}Installing/updating Node-Red modules...${NC}"
+# install Node-RED modules
+echo -e "${Cyan}Installing/updating Node-RED modules...${NC}"
 cd ~/.node-red
 rm package.json
 rm package-lock.json
@@ -119,8 +127,8 @@ echo -e "${Yellow}It may look frozen, but it is not! Please leave it running and
 echo -e "${Yellow}Go grab a coffee and relax for a while, I'll take care of the rest :)${NC}"
 npm install
 
-#install Node-Red Pekaway VanPi flows
-echo -e "${Cyan}Installing/updating Node-Red Pekaway VanPi flows...${NC}"
+# install Node-RED Pekaway VanPi flows
+echo -e "${Cyan}Installing/updating Node-RED Pekaway VanPi flows...${NC}"
 cd ~/pekaway
 wget ${Server}flows.json
 cp flows.json ~/.node-red/flows_pekaway.json
@@ -131,14 +139,14 @@ mv ~/.node-red/node_modules/node-red-dashboard/dist/icon120x120.png ~/.node-red/
 mv ~/.node-red/node_modules/node-red-dashboard/dist/icon192x192.png ~/.node-red/node_modules/node-red-dashboard/dist/icon192x192_old.png
 unzip icons.zip
 
-#install usbreset.c
+# install usbreset.c
 echo -e "${Cyan}Installing and compiling usbreset.c...${NC}"
 cd ~/
 wget ${Server}usbreset.c
 sudo gcc usbreset.c -o usbreset
 sudo mv usbreset /usr/local/sbin/
 
-# Install and configure Access Point
+# install and configure Access Point
 echo -e "${Cyan}Installing and configuring Access Point mode...${NC}"
 cd ~/
 sudo apt install hostapd dnsmasq -y
@@ -155,7 +163,7 @@ echo -e "# Listening interface\ninterface=wlan0\n# Pool of IP addresses served v
 sudo rfkill unblock wlan
 echo -e "country_code=DE\ninterface=wlan0\n\nssid=PeKaWayControl\nhw_mode=g\nchannel=6\nmacaddr_acl=0\nauth_algs=1\nwpa=2\nwpa_passphrase=pekawayfetzt\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" | sudo tee /etc/hostapd/hostapd.conf
 
-# Install and configure Nginx
+# install and configure Nginx
 echo -e "${Cyan}Installing and configuring Nginx${NC}"
 sudo apt update && sudo apt install nginx -y
 echo -e "server {\n   listen 80;\n   server_name peka.way pekaway.local vanpi.local van.pi;\n   location / {\n      proxy_pass http://127.0.0.1:1880/ui/;\n   }\n}\nserver {\n   listen 80;\n   server_name homebridge.peka.way hb.peka.way homebridge.van.pi hb.van.pi;\n   location / {\n      proxy_pass http://127.0.0.1:8581/;\n   }\n}" | sudo tee /etc/nginx/sites-available/pekaway1
@@ -163,21 +171,21 @@ sudo ln -s /etc/nginx/sites-available/pekaway1 /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
 sudo systemctl enable nginx
 
-# Install Mosquitto MQTT Server (probably already installed from packages.txt)
-echo -e "${Cyan}Installing Mosquitto MQTT Server${NC}"
+# install Mosquitto MQTT Server (probably already installed from packages.txt)
+echo -e "${Cyan}Installing Mosquitto MQTT broker${NC}"
 sudo apt install mosquitto mosquitto-clients -y
 sudo mkdir /var/log/mosquitto
 sudo touch /var/log/mosquitto/mosquitto.log
 sudo chmod 0755 /var/log/mosquitto/mosquitto.log
 
-# Implementing new udev rules and restarting udev service
+# implementing new udev rules and restarting udev service
 echo -e "${Cyan}Implementing udev rules for serial connections{NC}"
 sudo touch /etc/udev/rules.d/98-pekaway-tty.rules
 echo -e 'KERNEL=="ttyUSB*", KERNELS=="1-1.1", SYMLINK+="ttyPKW1"\nKERNEL=="ttyUSB*", KERNELS=="1-1.2", SYMLINK+="ttyPKW2"\nKERNEL=="ttyUSB*", KERNELS=="1-1.3", SYMLINK+="ttyPKW3"\nKERNEL=="ttyUSB*", KERNELS=="1-1.4", SYMLINK+="ttyPKW4"' | sudo tee /etc/udev/rules.d/98-pekaway-tty.rules
 sudo cp /etc/udev/rules.d/98-pekaway-tty.rules /lib/udev/rules.d/98-pekaway-tty.rules
 sudo udevadm control --reload-rules & sudo systemctl restart udev.service
 
-# Install Homebridge
+# install Homebridge
 echo -e "${Cyan}Installing and configuring Homebridge for Apple Homekit${NC}"
 sudo npm install -g --unsafe-perm homebridge homebridge-config-ui-x
 sudo hb-service install --user homebridge
@@ -187,7 +195,7 @@ cd ~/pekaway
 wget ${Server}config.json
 sudo \cp -r ~/pekaway/config.json /var/lib/homebridge/config.json
 
-# Install Zigbee2MQTT
+# install Zigbee2MQTT
 echo -e "${Cyan}Installing Zigbee2MQTT${NC}"
 sudo mkdir /opt/zigbee2mqtt
 sudo chown -R ${USER}: /opt/zigbee2mqtt
@@ -199,9 +207,8 @@ curl ${Server}configuration.yaml > /opt/zigbee2mqtt/data/configuration.yaml
 sudo wget ${Server}zigbee2mqtt.service
 sudo mv zigbee2mqtt.service /etc/systemd/system/
 
-# Clear files
+# clear files
 echo -e "${Cyan}Clearing folders and files...${NC}"
-sudo rm /tmp/wiringpi-latest.deb
 sudo rm ~/pekaway/home_pi_pekaway_files.zip
 sudo rm ~/pekaway/packages.txt
 sudo rm ~/pekaway/packages_bullseye.txt
@@ -209,7 +216,7 @@ sudo rm ~/pekaway/packages_buster.txt
 sudo rm ~/pekaway/pip3list.txt
 sudo rm ~/pekaway/flows.json
 
-# Restart Services
+# restart Services
 echo -e "${Cyan}Restarting services...${NC}"
 sudo systemctl daemon-reload
 echo -e "${Cyan}zigbee2mqtt.service is not started/enabled by default!${NC}"
@@ -221,7 +228,8 @@ sudo swapoff -a
 sleep 5
 sudo service dphys-swapfile stop
 sudo systemctl disable dphys-swapfile
-# Configure /boot/cmdline.txt
+
+# configure /boot/cmdline.txt
 echo -e "${Cyan}Configuring cmdline.txt...${NC}"
 sudo sed -i 's/^.*root=PARTUUID/root=PARTUUID/' /boot/cmdline.txt
 sed -i 's/flows.json/flows_pekaway.json/g' ~/.node-red/settings.js
@@ -236,23 +244,25 @@ echo -e "${Cyan}Script started: ${NC}${startdate}"
 echo -e "${Cyan}Script ended: ${NC}${enddate}"
 echo -e "${Red}Script runtime in Seconds: ${NC}${runtime}"
 
-# Reboot Raspberry Pi
+# reboot Raspberry Pi
 echo -e "${Yellow}Installation done, reboot needed!${NC}"
 echo -e "${Red}If connection is lost, RPI will reboot into Access Point Mode automatically, \nPlease connect to the access point (VanPiControl_xxx) and proceed from there${NC}"
 echo -e "${Red}--> logfile is saved at ${LOG_FILE}${NC}"
 echo "yes" > ~/pekaway/firstboot
-read -r -p "Do you want to reboot now? [y/n]" input
- 
-case $input in
-      [yY][eE][sS]|[yY])
-            sudo reboot
-            ;;
-      [nN][oO]|[nN])
-            echo "Reboot cancelled, please remember to reboot for the VanPi system to work properly!"
-            exit 0
-            ;;
-      *)
-            echo "Invalid input... please type 'sudo reboot' to reboot"
-            exit 0
-            ;;
-esac
+
+while true; do
+	read -r -p "Do you want to reboot now? [y/n]" input
+	 
+	case $input in
+		  [yY][eE][sS]|[yY])
+				sudo reboot
+				break;;
+		  [nN][oO]|[nN])
+				echo "Reboot cancelled, please remember to reboot for the VanPi system to work properly!"
+				exit
+				;;
+		  *)
+				echo "Invalid input... please type 'y' (yes) or 'n' (no)"
+				;;
+	esac
+done
